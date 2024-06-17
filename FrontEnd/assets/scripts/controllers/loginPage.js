45f885894da41081d05f1ad8c9bb36822a57a1a0
amarchive <antoine.marchive@orange.fr>
2024-06-17 15:31:58 +0200

    preparation fct add.eventListener

---
+++ FrontEnd/assets/scripts/controllers/loginPage.js
@@ -0,0 +1,19 @@
+//récupération des données
+// création de mes variables
+
+const email = document.getElementById("email");
+const password = document.getElementById("password");
+const form = document.querySelector("form");
+const errorText = document.getElementById("errortext");
+
+// ajout d'une ecouteur d'évènements au niveau de l'email et du password
+form.addEventListener("submit", () => {
+   const valueEmail = email.value;
+   const valuePassword = password.value;
+   if () => {
+
+   } else {
+
+   }
+   
+})
