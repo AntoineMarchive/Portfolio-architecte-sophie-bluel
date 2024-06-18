4cbc3af9d0b83fc6768585c3eeec7d19390ae6fb
amarchive <antoine.marchive@orange.fr>
2024-06-18 10:00:16 +0200

    change text

--- FrontEnd/assets/scripts/controllers/loginPage.js
+++ FrontEnd/assets/scripts/controllers/loginPage.js
@@ -4,7 +4,7 @@
 const email = document.getElementById("email");
 const password = document.getElementById("password");
 const form = document.querySelector("form");
-const errorText = document.getElementById("errortext");
+
 
 // ajout d'une ecouteur d'évènements au niveau de l'email et du password
 form.addEventListener("submit", () => {
@@ -16,4 +16,4 @@ form.addEventListener("submit", () => {
 
    }
    
-})
+});
