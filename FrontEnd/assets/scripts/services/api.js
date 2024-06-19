306874b98631fdb289a717762f4f3e87a331c150
amarchive <antoine.marchive@orange.fr>
2024-06-19 13:09:16 +0200

    finish async function

--- FrontEnd/assets/scripts/services/api.js
+++ FrontEnd/assets/scripts/services/api.js
@@ -1,11 +1,13 @@
 export async function getWorks() {
    const responseWorks = await fetch("http://localhost:5678/api/works"); // interroger le serveur
-   const works = await response.json(); // si reponse ok, on aura les données
+   const works = await responseWorks.json(); // si reponse ok, on aura les données
+   console.log(works);
    return works; // renvois les works
 };
 
 export async function getCategories() {
-    const response = await fetch("http://localhost:5678/api/categories");
-    const categories = await reponse.json();
+    const responseCategories = await fetch("http://localhost:5678/api/categories");
+    const categories = await responseCategories.json();
+    console.log(categories);
     return categories;
 };
