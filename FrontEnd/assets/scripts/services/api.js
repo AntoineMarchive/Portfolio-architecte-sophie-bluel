c5931ed6cbc5abecd1f6bb21981f316b6d49ae80
amarchive <antoine.marchive@orange.fr>
2024-07-02 15:18:56 +0200

    add function for connexion

--- FrontEnd/assets/scripts/services/api.js
+++ FrontEnd/assets/scripts/services/api.js
@@ -3,11 +3,25 @@ export async function getWorks() {
    const works = await responseWorks.json(); // si reponse ok, on aura les données
    //console.log(works);
    return works; // renvois les works
-}
+};
 
 export async function getCategories() {
     const responseCategories = await fetch("http://localhost:5678/api/categories");
     const categories = await responseCategories.json();
     //console.log(categories);
     return categories; // renvois les catégories
+};
+
+// export de la fonction pour email et password vers le serveur
+export async function getConnection(email, password) {
+    return fetch("http://localhost:5678/api/users/login", {
+        method: 'POST',
+        headers: {
+            "Content-type": "application/json"
+        },
+        body: JSON.stringify({
+            "email": email,
+            "password": password,
+        })
+    })
 };
