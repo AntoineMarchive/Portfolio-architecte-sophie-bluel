a05cd90867ced420295b40831d9cf3d3980e5520
amarchive <antoine.marchive@orange.fr>
2024-07-03 09:13:09 +0200

    small modifications

--- FrontEnd/assets/scripts/controllers/loginPage.js
+++ FrontEnd/assets/scripts/controllers/loginPage.js
@@ -1,25 +1,24 @@
 import { getConnection } from "../services/api.js";
 
-const email = document.getElementById("email")
-const password = document.getElementById("password")
-const form = document.querySelector("form")
-const errorText = document.getElementById("errortext")
+const email = document.getElementById("email");
+const password = document.getElementById("password");
+const form = document.querySelector("form");
+const errorText = document.getElementById("errortext");
  
  
 
 form.addEventListener("submit", (event) => {
     event.preventDefault()
-    const valueEmail = email.value
-    const valuePassword = password.value
+    const valueEmail = email.value;
+    const valuePassword = password.value;
     getConnection(valueEmail, valuePassword)
     .then((response) => response.json())
     .then(login => { 
         if (login.token) {
-            localStorage.setItem("token", login.token)
-            window.location.href="./index.html"
+            localStorage.setItem("token", login.token);
+            window.location.href="./index.html";
         } else {
-            errorText.classList.remove("hidden")
-        }
-    }) 
-    
+            errorText.classList.remove("hidden");
+        };
+    });
 });
